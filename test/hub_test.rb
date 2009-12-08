c70c3f92a1064c159c7ea323528803c3e6c63965
Chris Wanstrath <chris@ozmm.org>
2009-12-07 22:32:05 -0800

    document test helpers

--- test/hub_test.rb
+++ test/hub_test.rb
@@ -2,15 +2,21 @@ require 'test/unit'
 load File.dirname(__FILE__) + '/../bin/hub'
 
 class HubTest < Test::Unit::TestCase
-
+  # Shortcut for creating a `Hub` instance. Pass it what you would
+  # normally pass `hub` on the command line, e.g.
   #
-  # Test helpers
-  #
-
+  # shell: hub clone rtomayko/tilt
+  #  test: Hub("clone rtomayko/tilt")
   def Hub(args)
     Hub.new(*args.split(' '))
   end
 
+  # Shortcut for running the `hub` command in a subprocess. Returns
+  # STDOUT as a string. Pass it what you would normall pass `hub` on
+  # the command line, e.g.
+  #
+  # shell: hub clone rtomayko/tilt
+  #  test: hub("clone rtomayko/tilt")
   def hub(args)
     parent_read, child_write = IO.pipe
 
@@ -23,10 +29,19 @@ class HubTest < Test::Unit::TestCase
     parent_read.read
   end
 
+  # Asserts that `hub` will run a specific git command based on
+  # certain input.
+  #
+  # e.g.
+  #  assert_command "clone git/hub", "git clone git://github.com/git/hub.git"
+  #
+  # Here we are saying that this:
+  #   $ hub clone git/hub
+  # Should in turn execute this:
+  #   $ git clone git://github.com/git/hub.git
   def assert_command(input, expected)
     assert_equal expected, Hub(input).command
   end
-
 
   #
   # Assertions
