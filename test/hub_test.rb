414eafe95cd904bb131dcbe929ac1ba0ce1cb2e4
Chris Wanstrath <chris@ozmm.org>
2010-03-07 19:03:56 -0800

    whatever

--- test/hub_test.rb
+++ test/hub_test.rb
@@ -206,7 +206,6 @@ class HubTest < Test::Unit::TestCase
   def test_help_hub
     help_manpage = hub("help hub")
     assert_includes "git + hub = github", help_manpage
-    assert_includes "$ git clone schacon/ticgit", help_manpage
     assert_includes "Chris Wanstrath :: chris@ozmm.org", help_manpage
     assert_includes <<-config, help_manpage
 Use git-config(1) to display the currently configured GitHub username:
