8957c6ccbc9241511b6f8111833319ccba94b835
Jingwen Owen Ou <jingweno@gmail.com>
2013-05-23 11:50:09 -0700

    Add basic auth

--- git_hub.go
+++ git_hub.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/howeyc/gopass"
 	"io/ioutil"
 	"net/http"
-	"net/url"
 	"os"
 	"path/filepath"
 )
 
 const (
-	GitHubUrl string = "https://api.github.com"
+	GitHubUrl  string = "https://" + GitHubHost
+	GitHubHost string = "api.github.com"
 )
 
-type unprocessableEntityError struct {
+type GitHubError struct {
 	Resource string `json:"resource"`
 	Field    string `json:"field"`
 	Value    string `json:"value"`
@@ -24,53 +26,106 @@ type unprocessableEntityError struct {
 	Message  string `json:"message"`
 }
 
-type unprocessableEntity struct {
-	Message string                     `json:"message"`
-	Errors  []unprocessableEntityError `json:"errors"`
+type GitHubErrors struct {
+	Message string        `json:"message"`
+	Errors  []GitHubError `json:"errors"`
 }
 
 func NewGitHub() *GitHub {
 	configFile := filepath.Join(os.Getenv("HOME"), ".config", "gh")
 	config, _ := LoadConfig(configFile)
 
-	return &GitHub{&http.Client{}, config.Token}
+	user := config.User
+	if len(user) == 0 {
+		user = FetchGitOwner()
+	}
+
+	auth := config.Token
+	if len(auth) > 0 {
+		auth = "token " + auth
+	}
+
+	return &GitHub{&http.Client{}, user, "", auth}
+}
+
+func hashAuth(u, p string) string {
+	var a = fmt.Sprintf("%s:%s", u, p)
+	return base64.StdEncoding.EncodeToString([]byte(a))
 }
 
 type GitHub struct {
 	httpClient    *http.Client
+	User          string
+	Password      string
 	Authorization string
 }
 
-func (gh *GitHub) performBasicAuth(url *url.URL) {
-	url.String()
+func (gh *GitHub) performBasicAuth() error {
+	msg := fmt.Sprintf("%s password for %s (never stored): ", GitHubHost, gh.User)
+	fmt.Print(msg)
+
+	pass := gopass.GetPasswd()
+	if len(pass) == 0 {
+		return errors.New("Password cannot be empty.")
+	}
+	gh.Password = string(pass)
+
+	return gh.obtainOAuthToken()
 }
 
-func (gh *GitHub) call(request *http.Request) (*http.Response, error) {
-	if len(gh.Authorization) == 0 {
-		gh.performBasicAuth(request.URL)
+func (gh *GitHub) obtainOAuthToken() error {
+	gh.Authorization = fmt.Sprintf("Basic %s", hashAuth(gh.User, gh.Password))
+	response, err := gh.httpGet("/authorizations", nil)
+	if err != nil {
+		return err
 	}
 
-	request.Header.Set("Authorization", "token "+gh.Authorization)
+	fmt.Println(response.StatusCode)
+	js, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(js))
+
+	return nil
+}
+
+func (gh *GitHub) performRequest(request *http.Request) (*http.Response, error) {
+	if len(gh.Authorization) == 0 {
+		err := gh.performBasicAuth()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	request.Header.Set("Authorization", gh.Authorization)
 
 	response, err := gh.httpClient.Do(request)
 	if err != nil {
 		return response, err
 	}
 
-	if response.StatusCode != 422 {
+	if response.StatusCode >= 200 && response.StatusCode < 400 {
 		return response, err
 	}
 
+	err = handleGitHubErrors(response)
+
+	return response, err
+}
+
+func handleGitHubErrors(response *http.Response) error {
 	body, err := ioutil.ReadAll(response.Body)
 	if err == nil {
-		var unprocessable unprocessableEntity
-		err = json.Unmarshal(body, &unprocessable)
+		var githubErrors GitHubErrors
+		err = json.Unmarshal(body, &githubErrors)
 		if err != nil {
-			return response, err
+			return err
 		}
 
-		errorMessages := make([]string, len(unprocessable.Errors))
-		for _, e := range unprocessable.Errors {
+		errorMessages := make([]string, len(githubErrors.Errors))
+		for _, e := range githubErrors.Errors {
 			switch e.Code {
 			case "custom":
 				errorMessages = append(errorMessages, e.Message)
@@ -83,7 +138,7 @@ func (gh *GitHub) call(request *http.Request) (*http.Response, error) {
 			}
 		}
 
-		text := ""
+		var text string
 		for _, m := range errorMessages {
 			if len(m) > 0 {
 				text = text + m + "\n"
@@ -91,13 +146,29 @@ func (gh *GitHub) call(request *http.Request) (*http.Response, error) {
 		}
 
 		if len(text) == 0 {
-			text = unprocessable.Message
+			text = githubErrors.Message
 		}
 
 		err = errors.New(text)
 	}
 
-	return response, err
+	return err
+}
+
+func (gh *GitHub) httpGet(uri string, extraHeaders map[string]string) (*http.Response, error) {
+	url := fmt.Sprintf("%s%s", GitHubUrl, uri)
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if extraHeaders != nil {
+		for h, v := range extraHeaders {
+			request.Header.Set(h, v)
+		}
+	}
+
+	return gh.performRequest(request)
 }
 
 func (gh *GitHub) httpPost(uri string, extraHeaders map[string]string, content *bytes.Buffer) (*http.Response, error) {
@@ -115,7 +186,7 @@ func (gh *GitHub) httpPost(uri string, extraHeaders map[string]string, content *
 
 	request.Header.Set("Content-Type", "application/json; charset=utf-8")
 
-	return gh.call(request)
+	return gh.performRequest(request)
 }
 
 type PullRequestParams struct {
@@ -140,7 +211,7 @@ func (gh *GitHub) CreatePullRequest(owner, repo string, params PullRequestParams
 	}
 
 	buffer := bytes.NewBuffer(b)
-	url := "/repos/" + owner + "/" + repo + "/pulls"
+	url := fmt.Sprintf("/repos/%s/%s/pulls", owner, repo)
 	response, err := gh.httpPost(url, nil, buffer)
 	if err != nil {
 		return nil, err
