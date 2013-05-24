213b8cc8c7821de70c9bdef593552f6e43272f07
Jingwen Owen Ou <jingweno@gmail.com>
2013-05-23 17:44:28 -0700

    Remove TestLoadConfig

--- config_test.go
+++ config_test.go
@@ -3,15 +3,9 @@ package main
 import (
 	"github.com/bmizerany/assert"
 	"os"
+	"path/filepath"
 	"testing"
 )
-
-func TestLoadConfig(t *testing.T) {
-	config, _ := LoadConfig("./test_support/gh")
-
-	assert.Equal(t, "jingweno", config.User)
-	assert.Equal(t, "02a66f3bdde949182bc0d629f1abef0d501e6a53", config.Token)
-}
 
 func TestSaveConfig(t *testing.T) {
 	config := Config{"jingweno", "123"}
@@ -24,5 +18,5 @@ func TestSaveConfig(t *testing.T) {
 	assert.Equal(t, "jingweno", newConfig.User)
 	assert.Equal(t, "123", newConfig.Token)
 
-	os.Remove(file)
+	os.RemoveAll(filepath.Dir(file))
 }
