59c4683ec2c15294bb7779891474475a4a3dc50f
Jingwen Owen Ou <jingweno@gmail.com>
2013-06-13 00:17:08 -0600

    Update gh.rb

--- homebrew/gh.rb
+++ homebrew/gh.rb
@@ -2,22 +2,21 @@ require 'formula'
 
 class Gh < Formula
   VERSION = '0.6.1'
-  ARCH = if MacOS.prefer_64_bit?
-           'amd64'
-         else
-           '386'
-         end
 
   homepage 'https://github.com/jingweno/gh'
   url "https://github.com/jingweno/gh/archive/#{VERSION}.tar.gz"
+  sha1 '1e4ca70ebf018ae192a641f18b735beca5df5c31'
   version VERSION
 
   head 'https://github.com/jingweno/gh.git'
 
+  depends_on 'hg'
   depends_on 'go'
 
   def install
-    system 'go build -o gh'
+    go_path = `pwd`.chomp
+    system "GOPATH='#{go_path}' go get -d ./..."
+    system "GOPATH='#{go_path}' go build -o gh"
     bin.install 'gh'
   end
 
