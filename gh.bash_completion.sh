fb1d6809b496966c2f704f649c886f9d82b7a8df
Jingwen Owen Ou <jingweno@gmail.com>
2013-06-29 12:29:23 -0700

    Add bash and zsh completion script

---
+++ gh.bash_completion.sh
@@ -0,0 +1,25 @@
+# hub tab-completion script for bash.
+# This script complements the completion script that ships with git.
+
+# Check that git tab completion is available
+if declare -F _git > /dev/null; then
+  # Duplicate and rename the 'list_all_commands' function
+  eval "$(declare -f __git_list_all_commands | \
+        sed 's/__git_list_all_commands/__git_list_all_commands_without_hub/')"
+
+  # Wrap the 'list_all_commands' function with extra hub commands
+  __git_list_all_commands() {
+    cat <<-EOF
+alias
+pr
+fork
+create
+browse
+compare
+EOF
+    __git_list_all_commands_without_hub
+  }
+
+  # Ensure cached commands are cleared
+  __git_all_commands=""
+fi
