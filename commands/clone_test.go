46cf7a2f21751b6a33e116dae84fc7de0041e1ce
Jingwen Owen Ou <jingweno@gmail.com>
2013-07-01 10:58:31 -0700

    Add test for hyphenized name

--- commands/clone_test.go
+++ commands/clone_test.go
@@ -3,9 +3,9 @@ package commands
 import (
 	"github.com/bmizerany/assert"
 	"github.com/jingweno/gh/github"
-	"testing"
-  "os"
+	"os"
 	"path/filepath"
+	"testing"
 )
 
 func TestTransformCloneArgs(t *testing.T) {
@@ -21,10 +21,16 @@ func TestTransformCloneArgs(t *testing.T) {
 	assert.Equal(t, 1, args.Size())
 	assert.Equal(t, "git@github.com:jingweno/gh.git", args.First())
 
-  github.DefaultConfigFile = "./test_support/gh"
+	args = NewArgs([]string{"-p", "acl-services/devise-acl"})
+	transformCloneArgs(args)
+
+	assert.Equal(t, 1, args.Size())
+	assert.Equal(t, "git@github.com:acl-services/devise-acl.git", args.First())
+
+	github.DefaultConfigFile = "./test_support/gh"
 	config := github.Config{User: "jingweno", Token: "123"}
 	github.SaveConfig(&config)
-  defer os.RemoveAll(filepath.Dir(github.DefaultConfigFile))
+	defer os.RemoveAll(filepath.Dir(github.DefaultConfigFile))
 
 	args = NewArgs([]string{"-p", "jekyll_and_hyde"})
 	transformCloneArgs(args)
