cd63eec5ebc9ddc418b4d833ea85213b26b4733d
Jingwen Owen Ou <jingweno@gmail.com>
2013-07-05 13:58:51 -0700

    Change help menu

--- commands/help.go
+++ commands/help.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/jingweno/gh/utils"
 	"os"
-	"text/template"
 )
 
 var cmdHelp = &Command{
@@ -34,39 +33,61 @@ func runHelp(cmd *Command, args *Args) {
 		}
 	}
 
-	fmt.Fprintf(os.Stderr, "Unknown help topic: %q. Run 'gh help'.\n", args.FirstParam())
+	fmt.Fprintf(os.Stderr, "Unknown help topic: %q. Run 'git help'.\n", args.FirstParam())
 	os.Exit(2)
 }
 
-var usageTemplate = template.Must(template.New("usage").Parse(`Usage: gh [command] [options] [arguments]
+var helpText = `usage: git [--version] [--exec-path[=<path>]] [--html-path] [--man-path] [--info-path]
+           [-p|--paginate|--no-pager] [--no-replace-objects] [--bare]
+           [--git-dir=<path>] [--work-tree=<path>] [--namespace=<name>]
+           [-c name=value] [--help]
+           <command> [<args>]
 
-Branching Commands:{{range .BasicCommands}}{{if .Runnable}}{{if .List}}
-    {{.Name | printf "%-16s"}}  {{.Short}}{{end}}{{end}}{{end}}
+Basic Commands:
+   init       Create an empty git repository or reinitialize an existing one
+   add        Add new or modified files to the staging area
+   rm         Remove files from the working directory and staging area
+   mv         Move or rename a file, a directory, or a symlink
+   status     Show the status of the working directory and staging area
+   commit     Record changes to the repository
 
-Branching Commands:{{range .BranchingCommands}}{{if .Runnable}}{{if .List}}
-    {{.Name | printf "%-16s"}}  {{.Short}}{{end}}{{end}}{{end}}
+History Commands:
+   log        Show the commit history log
+   diff       Show changes between commits, commit and working tree, etc
+   show       Show information about commits, tags or files
 
-Remote Commands:{{range .RemoteCommands}}{{if .Runnable}}{{if .List}}
-    {{.Name | printf "%-16s"}}  {{.Short}}{{end}}{{end}}{{end}}
+Branching Commands:
+   branch     List, create, or delete branches
+   checkout   Switch the active branch to another branch
+   merge      Join two or more development histories (branches) together
+   tag        Create, list, delete, sign or verify a tag object
 
-GitHub Commands:{{range .GitHubCommands}}{{if .Runnable}}{{if .List}}
-    {{.Name | printf "%-16s"}}  {{.Short}}{{end}}{{end}}{{end}}
+Remote Commands:
+   clone      Clone a remote repository into a new directory
+   fetch      Download data, tags and branches from a remote repository
+   pull       Fetch from and merge with another repository or a local branch
+   push       Upload data, tags and branches to a remote repository
+   remote     View and manage a set of remote repositories
 
-See 'gh help [command]' for more information about a command.
-`))
+Advanced Commands:
+   reset      Reset your staging area or working directory to another point
+   rebase     Re-apply a series of patches in one branch onto another
+   bisect     Find by binary search the change that introduced a bug
+   grep       Print files with lines matching a pattern in your codebase
+
+GitHub Commands:
+   pull-request   Open a pull request on GitHub
+   fork           Make a fork of a remote repository on GitHub and add as remote
+   create         Create this repository on GitHub and add GitHub as origin
+   browse         Open a GitHub page in the default browser
+   compare        Open a compare page on GitHub
+   ci-status      Show the CI status of a commit
+
+See 'git help <command>' for more information on a specific command.
+`
 
 func printUsage() {
-	usageTemplate.Execute(os.Stdout, struct {
-		BasicCommands     []*Command
-		BranchingCommands []*Command
-		RemoteCommands    []*Command
-		GitHubCommands    []*Command
-	}{
-		Basic,
-		Branching,
-		Remote,
-		GitHub,
-	})
+	fmt.Print(helpText)
 }
 
 func usage() {
