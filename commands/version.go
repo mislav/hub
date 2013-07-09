3b53e51811ebbaa0704d212ba29a99dd7a4fa7cf
Jingwen Owen Ou <jingweno@gmail.com>
2013-07-09 14:58:31 -0700

    Bump version to v0.14.0

--- commands/version.go
+++ commands/version.go
@@ -7,7 +7,7 @@ import (
 	"os"
 )
 
-const Version = "0.13.1"
+const Version = "0.14.0"
 
 var cmdVersion = &Command{
 	Run:   runVersion,
--- homebrew/gh.rb
+++ homebrew/gh.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 class Gh < Formula
-  VERSION = "0.13.1"
+  VERSION = "0.14.0"
   ARCH = if MacOS.prefer_64_bit?
            "amd64"
          else
