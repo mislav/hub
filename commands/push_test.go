b33a5917e756c2484433b6abc69667a7df4b6d98
Jingwen Owen Ou <jingweno@gmail.com>
2013-08-16 15:35:46 -0700

    Comment out test doesn't pass on travis

--- commands/push_test.go
+++ commands/push_test.go
@@ -2,11 +2,10 @@ package commands
 
 import (
 	"github.com/bmizerany/assert"
-	"regexp"
 	"testing"
 )
 
-func _TestTransformPushArgs(t *testing.T) {
+func TestTransformPushArgs(t *testing.T) {
 	args := NewArgs([]string{"push", "origin,staging,qa", "bert_timeout"})
 	transformPushArgs(args)
 	cmds := args.Commands()
@@ -15,11 +14,12 @@ func _TestTransformPushArgs(t *testing.T) {
 	assert.Equal(t, "git push origin bert_timeout", cmds[0].String())
 	assert.Equal(t, "git push staging bert_timeout", cmds[1].String())
 
-	args = NewArgs([]string{"push", "origin"})
-	transformPushArgs(args)
-	cmds = args.Commands()
+	// TODO: travis-ci doesn't have HEAD
+	//args = NewArgs([]string{"push", "origin"})
+	//transformPushArgs(args)
+	//cmds = args.Commands()
 
-	assert.Equal(t, 1, len(cmds))
-	pushRegexp := regexp.MustCompile("git push origin .+")
-	assert.T(t, pushRegexp.MatchString(cmds[0].String()))
+	//assert.Equal(t, 1, len(cmds))
+	//pushRegexp := regexp.MustCompile("git push origin .+")
+	//assert.T(t, pushRegexp.MatchString(cmds[0].String()))
 }
