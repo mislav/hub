24296807131a9e1e52abc08bd268a2ca9205a446
Jingwen Owen Ou <jingweno@gmail.com>
2013-09-08 09:11:49 -0700

    Properly fix transform logic for push

--- commands/push_test.go
+++ commands/push_test.go
@@ -5,6 +5,17 @@ import (
 	"testing"
 )
 
+func testPush(t *testing.T) {
+	args := NewArgs([]string{"push", "origin,staging,qa", "bert_timeout"})
+	push(nil, args)
+
+	cmds := args.Commands()
+
+	assert.Equal(t, 3, len(cmds))
+	assert.Equal(t, "git push origin bert_timeout", cmds[0].String())
+	assert.Equal(t, "git push staging bert_timeout", cmds[1].String())
+}
+
 func TestTransformPushArgs(t *testing.T) {
 	args := NewArgs([]string{"push", "origin,staging,qa", "bert_timeout"})
 	transformPushArgs(args)
