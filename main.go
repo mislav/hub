fa8f2c95a7b75cc685984bba3dfe1ae9adf282b8
Jingwen Owen Ou <jingweno@gmail.com>
2013-09-22 17:51:37 -0700

    Spawning processes to run git commands; Exec(1) doesn't work on Windows...

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"github.com/jingweno/gh/commands"
-	"github.com/jingweno/gh/utils"
 	"os"
 )
 
 func main() {
 	runner := commands.Runner{Args: os.Args[1:]}
 	err := runner.Execute()
-	utils.Check(err)
+	os.Exit(err.ExitCode)
 }
