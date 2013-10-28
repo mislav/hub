c3fe6784591c0b4446ec5041bdc8db096ae70b95
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2013-10-29 00:56:00 +0100

    `ci-status -v` prints the URL to CI build results
    Fixes #422, closes #423

--- lib/hub/commands.rb
+++ lib/hub/commands.rb
@@ -78,6 +78,7 @@ module Hub
     def ci_status(args)
       args.shift
       ref = args.words.first || 'HEAD'
+      verbose = args.include?('-v')
 
       unless head_project = local_repo.current_project
         abort "Aborted: the origin remote doesn't point to a GitHub repository."
@@ -89,7 +90,13 @@ module Hub
 
       statuses = api_client.statuses(head_project, sha)
       status = statuses.first
-      ref_state = status ? status['state'] : 'no status'
+      if status
+        ref_state = status['state']
+        ref_target_url = status['target_url']
+      else
+        ref_state = 'no status'
+        ref_target_url = nil
+      end
 
       exit_code = case ref_state
         when 'success'          then 0
@@ -98,7 +105,11 @@ module Hub
         else 3
         end
 
-      $stdout.puts ref_state
+      if verbose and ref_target_url
+        $stdout.puts "%s: %s" % [ref_state, ref_target_url]
+      else
+        $stdout.puts ref_state
+      end
       exit exit_code
     end
 
