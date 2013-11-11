1ad33e209086cd5c9a2021cd18ecd34552e578ba
Jingwen Owen Ou <jingweno@gmail.com>
2013-11-11 10:46:45 -0800

    Update to the latest octokit API

--- github/github.go
+++ github/github.go
@@ -16,13 +16,13 @@ type GitHub struct {
 }
 
 func (gh *GitHub) PullRequest(id string) (pr *octokit.PullRequest, err error) {
-	client := gh.octokit()
-	prService, err := client.PullRequests(&octokit.PullRequestsURL, octokit.M{"owner": gh.Project.Owner, "repo": gh.Project.Name, "number": id})
+	url, err := octokit.PullRequestsURL.Expand(octokit.M{"owner": gh.Project.Owner, "repo": gh.Project.Name, "number": id})
 	if err != nil {
 		return
 	}
 
-	pr, result := prService.Get()
+	client := gh.octokit()
+	pr, result := client.PullRequests(url).Get()
 	if result.HasError() {
 		err = result.Err
 	}
@@ -31,14 +31,14 @@ func (gh *GitHub) PullRequest(id string) (pr *octokit.PullRequest, err error) {
 }
 
 func (gh *GitHub) CreatePullRequest(base, head, title, body string) (pr *octokit.PullRequest, err error) {
-	client := gh.octokit()
-	prService, err := client.PullRequests(&octokit.PullRequestsURL, octokit.M{"owner": gh.Project.Owner, "repo": gh.Project.Name})
+	url, err := octokit.PullRequestsURL.Expand(octokit.M{"owner": gh.Project.Owner, "repo": gh.Project.Name})
 	if err != nil {
 		return
 	}
 
+	client := gh.octokit()
 	params := octokit.PullRequestParams{Base: base, Head: head, Title: title, Body: body}
-	pr, result := prService.Create(params)
+	pr, result := client.PullRequests(url).Create(params)
 	if result.HasError() {
 		err = result.Err
 	}
@@ -47,14 +47,14 @@ func (gh *GitHub) CreatePullRequest(base, head, title, body string) (pr *octokit
 }
 
 func (gh *GitHub) CreatePullRequestForIssue(base, head, issue string) (pr *octokit.PullRequest, err error) {
-	client := gh.octokit()
-	prService, err := client.PullRequests(&octokit.PullRequestsURL, octokit.M{"owner": gh.Project.Owner, "repo": gh.Project.Name})
+	url, err := octokit.PullRequestsURL.Expand(octokit.M{"owner": gh.Project.Owner, "repo": gh.Project.Name})
 	if err != nil {
 		return
 	}
 
+	client := gh.octokit()
 	params := octokit.PullRequestForIssueParams{Base: base, Head: head, Issue: issue}
-	pr, result := prService.Create(params)
+	pr, result := client.PullRequests(url).Create(params)
 	if result.HasError() {
 		err = result.Err
 	}
@@ -63,13 +63,13 @@ func (gh *GitHub) CreatePullRequestForIssue(base, head, issue string) (pr *octok
 }
 
 func (gh *GitHub) Repository(project Project) (repo *octokit.Repository, err error) {
-	client := gh.octokit()
-	repoService, err := client.Repositories(&octokit.RepositoryURL, octokit.M{"owner": project.Owner, "repo": project.Name})
+	url, err := octokit.RepositoryURL.Expand(octokit.M{"owner": project.Owner, "repo": project.Name})
 	if err != nil {
 		return
 	}
 
-	repo, result := repoService.Get()
+	client := gh.octokit()
+	repo, result := client.Repositories(url).Get()
 	if result.HasError() {
 		err = result.Err
 	}
@@ -92,14 +92,14 @@ func (gh *GitHub) CreateRepository(project Project, description, homepage string
 		repoURL = octokit.UserRepositoriesURL
 	}
 
-	client := gh.octokit()
-	repoService, err := client.Repositories(&repoURL, octokit.M{"org": project.Owner})
+	url, err := repoURL.Expand(octokit.M{"org": project.Owner})
 	if err != nil {
 		return
 	}
 
+	client := gh.octokit()
 	params := octokit.Repository{Name: project.Name, Description: description, Homepage: homepage, Private: isPrivate}
-	repo, result := repoService.Create(params)
+	repo, result := client.Repositories(url).Create(params)
 	if result.HasError() {
 		err = result.Err
 	}
@@ -108,13 +108,13 @@ func (gh *GitHub) CreateRepository(project Project, description, homepage string
 }
 
 func (gh *GitHub) Releases() (releases []octokit.Release, err error) {
-	client := gh.octokit()
-	releasesService, err := client.Releases(nil, octokit.M{"owner": gh.Project.Owner, "repo": gh.Project.Name})
+	url, err := octokit.ReleasesURL.Expand(octokit.M{"owner": gh.Project.Owner, "repo": gh.Project.Name})
 	if err != nil {
 		return
 	}
 
-	releases, result := releasesService.GetAll()
+	client := gh.octokit()
+	releases, result := client.Releases(url).GetAll()
 	if result.HasError() {
 		err = result.Err
 		return
@@ -124,10 +124,13 @@ func (gh *GitHub) Releases() (releases []octokit.Release, err error) {
 }
 
 func (gh *GitHub) CIStatus(sha string) (status *octokit.Status, err error) {
-	client := gh.octokit()
-	statusesService, err := client.Statuses(nil, octokit.M{"owner": gh.Project.Owner, "repo": gh.Project.Name, "ref": sha})
+	url, err := octokit.StatusesURL.Expand(octokit.M{"owner": gh.Project.Owner, "repo": gh.Project.Name, "ref": sha})
+	if err != nil {
+		return
+	}
 
-	statuses, result := statusesService.GetAll()
+	client := gh.octokit()
+	statuses, result := client.Statuses(url).GetAll()
 	if result.HasError() {
 		err = result.Err
 		return
@@ -149,9 +152,13 @@ func (gh *GitHub) ForkRepository(name, owner string, noRemote bool) (repo *octok
 		return
 	}
 
+	url, err := octokit.ForksURL.Expand(octokit.M{"owner": owner, "repo": name})
+	if err != nil {
+		return
+	}
+
 	client := gh.octokit()
-	repoService, err := client.Repositories(&octokit.ForksURL, octokit.M{"owner": owner, "repo": name})
-	repo, result := repoService.Create(nil)
+	repo, result := client.Repositories(url).Create(nil)
 	if result.HasError() {
 		err = result.Err
 	}
@@ -160,13 +167,13 @@ func (gh *GitHub) ForkRepository(name, owner string, noRemote bool) (repo *octok
 }
 
 func (gh *GitHub) Issues() (issues []octokit.Issue, err error) {
-	client := gh.octokit()
-	issuesService, err := client.Issues(&octokit.RepoIssuesURL, octokit.M{"owner": gh.Project.Owner, "repo": gh.Project.Name})
+	url, err := octokit.RepoIssuesURL.Expand(octokit.M{"owner": gh.Project.Owner, "repo": gh.Project.Name})
 	if err != nil {
 		return
 	}
 
-	issues, result := issuesService.GetAll()
+	client := gh.octokit()
+	issues, result := client.Issues(url).GetAll()
 	if result.HasError() {
 		err = result.Err
 		return
@@ -186,12 +193,14 @@ func (gh *GitHub) ExpandRemoteUrl(owner, name string, isSSH bool) (url string) {
 }
 
 func findOrCreateToken(user, password, twoFactorCode string) (token string, err error) {
-	basicAuth := octokit.BasicAuth{Login: user, Password: password, OneTimePassword: twoFactorCode}
-	client := octokit.NewClient(basicAuth)
-	authsService, err := client.Authorizations(nil, nil)
+	url, err := octokit.AuthorizationsURL.Expand(nil)
 	if err != nil {
 		return
 	}
+
+	basicAuth := octokit.BasicAuth{Login: user, Password: password, OneTimePassword: twoFactorCode}
+	client := octokit.NewClient(basicAuth)
+	authsService := client.Authorizations(url)
 
 	auths, result := authsService.GetAll()
 	if result.HasError() {
