400ae3485acf358876e0409c2be2d5d578e7eb64
Fuad Saud <fuadksd@gmail.com>
2013-12-17 23:04:13 -0200

    Extract Hub::Context::System to it's own file

--- lib/hub/context.rb
+++ lib/hub/context.rb
@@ -4,6 +4,7 @@ require 'uri'
 
 require_relative 'context/git_reader'
 require_relative 'context/git_reader_methods'
+require_relative 'context/system'
 
 module Hub
   # Methods for inspecting the environment, such as reading git config,
@@ -381,73 +382,6 @@ module Hub
       # avoid shellsplitting "C:\Program Files"
       if File.exist? editor then [editor]
       else editor.shellsplit
-      end
-    end
-
-    module System
-      # Cross-platform web browser command; respects the value set in $BROWSER.
-      #
-      # Returns an array, e.g.: ['open']
-      def browser_launcher
-        browser = ENV['BROWSER'] || (
-          osx? ? 'open' : windows? ? %w[cmd /c start] :
-          %w[xdg-open cygstart x-www-browser firefox opera mozilla netscape].find { |comm| which comm }
-        )
-
-        abort "Please set $BROWSER to a web launcher to use this command." unless browser
-        Array(browser)
-      end
-
-      def osx?
-        require 'rbconfig'
-        RbConfig::CONFIG['host_os'].to_s.include?('darwin')
-      end
-
-      def windows?
-        require 'rbconfig'
-        RbConfig::CONFIG['host_os'] =~ /msdos|mswin|djgpp|mingw|windows/
-      end
-
-      def unix?
-        require 'rbconfig'
-        RbConfig::CONFIG['host_os'] =~ /(aix|darwin|linux|(net|free|open)bsd|cygwin|solaris|irix|hpux)/i
-      end
-
-      # Cross-platform way of finding an executable in the $PATH.
-      #
-      #   which('ruby') #=> /usr/bin/ruby
-      def which(cmd)
-        exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
-        ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-          exts.each { |ext|
-            exe = "#{path}/#{cmd}#{ext}"
-            return exe if File.executable? exe
-          }
-        end
-        return nil
-      end
-
-      # Checks whether a command exists on this system in the $PATH.
-      #
-      # name - The String name of the command to check for.
-      #
-      # Returns a Boolean.
-      def command?(name)
-        !which(name).nil?
-      end
-
-      def tmp_dir
-        ENV['TMPDIR'] || ENV['TEMP'] || '/tmp'
-      end
-
-      def terminal_width
-        if unix?
-          width = %x{stty size 2>#{NULL}}.split[1].to_i
-          width = %x{tput cols 2>#{NULL}}.to_i if width.zero?
-        else
-          width = 0
-        end
-        width < 10 ? 78 : width
       end
     end
 
