mislav/hub

commit 06999d907e3e606b8bfdbf3037e9bb8b3e2a1ea8

Improve detecting of origin remote and remote branch
In hub lingo, "origin remote" is the remote pointing to the canonical
GitHub project, i.e. where pull requests should be sent. "Remote branch"
is where hub guesses the user has pushed their changes from the current
local branch.

The origin remote can now be called "upstream", "github", or "origin",
in that order of precedence.

The `browse`, `compare`, and especially `pull-request` commands need to
know the remote branch. Previously we relied on git upstream
configuration, but that wasn't enough. This behavior is now only kept if
git "push.default" is set to "upstream" or "tracking".

For "push.default" values "current", "simple" (git 2.0 default), or
"matching" (git 1.x default), we can safely assume that the user
probably pushes to the same-named branches on some remote, and we ignore
upstream configuration.

To find the remote where the user pushed their changes, we search for
the same-named branch on:

1. the remote which points to a GitHub project owned by the current user;
2. "origin", "github", and "upstream" remotes.

When a match is found, it is assumed that the user pushed there and it
is taken as the implicit pull-request head.

Fixes #158, fixes #360, fixes #381

lib/hub/commands.rb
@@ -121,7 +121,7 @@ module Hub
       options = { }
       force = explicit_owner = false
       base_project = local_repo.main_project
-      head_project = local_repo.current_project
+      tracked_branch, head_project = remote_branch_and_project(method(:github_user))
 
       unless current_branch
         abort "Aborted: not currently on any branch."
@@ -179,7 +179,7 @@ module Hub
       options[:project] = base_project
       options[:base] ||= master_branch.short_name
 
-      if tracked_branch = options[:head].nil? && current_branch.upstream
+      if options[:head].nil? && tracked_branch
         if !tracked_branch.remote?
           # The current branch is tracking another local branch. Pretend there is
           # no upstream configuration at all.
@@ -191,12 +191,6 @@ module Hub
         end
       end
       options[:head] ||= (tracked_branch || current_branch).short_name
-
-      # when no tracking, assume remote branch is published under active user's fork
-      user = github_user(head_project.host)
-      if head_project.owner != user and !tracked_branch and !explicit_owner
-        head_project = head_project.owned_by(user)
-      end
 
       remote_branch = "#{head_project.remote}/#{options[:head]}"
       options[:head] = "#{head_project.owner}:#{options[:head]}"
@@ -673,8 +667,8 @@ module Hub
           branch = master_branch
         else
           # $ hub browse
-          project = current_project
-          branch = current_branch && current_branch.upstream || master_branch
+          branch, project = remote_branch_and_project(method(:github_user))
+          branch ||= master_branch
         end
 
         abort "Usage: hub browse [<USER>/]<REPOSITORY>" unless project
@@ -704,11 +698,10 @@ module Hub
     def compare(args)
       args.shift
       browse_command(args) do
+        branch, project = remote_branch_and_project(method(:github_user))
         if args.empty?
-          branch = current_branch.upstream
           if branch and not branch.master?
             range = branch.short_name
-            project = current_project
           else
             abort "Usage: hub compare [USER] [<START>...]<END>"
           end
@@ -716,9 +709,9 @@ module Hub
           sha_or_tag = /((?:#{OWNER_RE}:)?\w[\w.-]+\w)/
           # replaces two dots with three: "sha1...sha2"
           range = args.pop.sub(/^#{sha_or_tag}\.\.#{sha_or_tag}$/, '\1...\2')
-          project = if owner = args.pop then github_project(nil, owner)
-                    else current_project
-                    end
+          if owner = args.pop
+            project = project.owned_by(owner)
+          end
         end
 
         project.web_url "/compare/#{range}"

features/browse.feature
@@ -1,4 +1,7 @@
 Feature: hub browse
+  Background:
+    Given I am "mislav" on github.com with OAuth token "OTOKEN"
+
   Scenario: Project with owner
     When I successfully run `hub browse mislav/dotfiles`
     Then there should be no output
@@ -49,6 +52,7 @@ Feature: hub browse
 
   Scenario: Current branch
     Given I am in "git://github.com/mislav/dotfiles.git" git repo
+    And git "push.default" is set to "upstream"
     And I am on the "feature" branch with upstream "origin/experimental"
     When I successfully run `hub browse`
     Then "open https://github.com/mislav/dotfiles/tree/experimental" should be run
@@ -74,12 +78,14 @@ Feature: hub browse
 
   Scenario: Commits on current branch
     Given I am in "git://github.com/mislav/dotfiles.git" git repo
+    And git "push.default" is set to "upstream"
     And I am on the "feature" branch with upstream "origin/experimental"
     When I successfully run `hub browse -- commits`
     Then "open https://github.com/mislav/dotfiles/commits/experimental" should be run
 
   Scenario: Complex branch
     Given I am in "git://github.com/mislav/dotfiles.git" git repo
+    And git "push.default" is set to "upstream"
     And I am on the "foo/bar" branch with upstream "origin/baz/qux/moo"
     When I successfully run `hub browse`
     Then "open https://github.com/mislav/dotfiles/tree/baz/qux/moo" should be run

features/compare.feature
@@ -1,6 +1,7 @@
-Feature: hub browse
+Feature: hub compare
   Background:
     Given I am in "git://github.com/mislav/dotfiles.git" git repo
+    And I am "mislav" on github.com with OAuth token "OTOKEN"
 
   Scenario: Compare branch
     When I successfully run `hub compare refactor`
@@ -27,6 +28,7 @@ Feature: hub browse
 
   Scenario: No args, has upstream branch
     Given I am on the "feature" branch with upstream "origin/experimental"
+    And git "push.default" is set to "upstream"
     When I successfully run `hub compare`
     Then there should be no output
     And "open https://github.com/mislav/dotfiles/compare/experimental" should be run

features/pull_request.feature
@@ -23,7 +23,7 @@ Feature: hub pull-request
       """
       post('/repos/Manganeez/repo/pulls') {
         assert :base  => 'master',
-               :head  => 'mislav:master',
+               :head  => 'Manganeez:master',
                :title => 'here we go'
         json :html_url => "https://github.com/Manganeez/repo/pull/12"
       }
@@ -375,4 +375,51 @@ Feature: hub pull-request
       }
       """
     When I successfully run `hub pull-request -m enterprisey`
+    Then the output should contain exactly "the://url\n"
+
+  Scenario: Create pull request from branch on the same remote
+    Given the "origin" remote has url "git://github.com/github/coral.git"
+    And the "mislav" remote has url "git://github.com/mislav/coral.git"
+    And I am on the "feature" branch pushed to "origin/feature"
+    Given the GitHub API server:
+      """
+      post('/repos/github/coral/pulls') {
+        assert :base  => 'master',
+               :head  => 'github:feature',
+               :title => 'hereyougo'
+        json :html_url => "the://url"
+      }
+      """
+    When I successfully run `hub pull-request -m hereyougo`
+    Then the output should contain exactly "the://url\n"
+
+  Scenario: Create pull request from branch on the personal fork
+    Given the "origin" remote has url "git://github.com/github/coral.git"
+    And the "doge" remote has url "git://github.com/mislav/coral.git"
+    And I am on the "feature" branch pushed to "doge/feature"
+    Given the GitHub API server:
+      """
+      post('/repos/github/coral/pulls') {
+        assert :base  => 'master',
+               :head  => 'mislav:feature',
+               :title => 'hereyougo'
+        json :html_url => "the://url"
+      }
+      """
+    When I successfully run `hub pull-request -m hereyougo`
+    Then the output should contain exactly "the://url\n"
+
+  Scenario: Create pull request to "upstream" remote
+    Given the "upstream" remote has url "git://github.com/github/coral.git"
+    And I am on the "master" branch pushed to "origin/master"
+    Given the GitHub API server:
+      """
+      post('/repos/github/coral/pulls') {
+        assert :base  => 'master',
+               :head  => 'mislav:master',
+               :title => 'hereyougo'
+        json :html_url => "the://url"
+      }
+      """
+    When I successfully run `hub pull-request -m hereyougo`
     Then the output should contain exactly "the://url\n"

features/steps.rb
@@ -12,6 +12,10 @@ Given(/^"([^"]*)" is a whitelisted Enterprise host$/) do |host|
   run_silent %(git config --global --add hub.host "#{host}")
 end
 
+Given(/^git "(.+?)" is set to "(.+?)"$/) do |key, value|
+  run_silent %(git config #{key} "#{value}")
+end
+
 Given(/^the "([^"]*)" remote has url "([^"]*)"$/) do |remote_name, url|
   remotes = run_silent('git remote').split("\n")
   unless remotes.include? remote_name
@@ -62,7 +66,7 @@ When(/^I make (a|\d+) commits?$/) do |num|
   num.times { empty_commit }
 end
 
-Given(/^I am on the "([^"]+)" branch(?: with upstream "([^"]+)")?$/) do |name, upstream|
+Given(/^I am on the "([^"]+)" branch(?: (pushed to|with upstream) "([^"]+)")?$/) do |name, type, upstream|
   empty_commit
   if upstream
     full_upstream = ".git/refs/remotes/#{upstream}"
@@ -71,7 +75,8 @@ Given(/^I am on the "([^"]+)" branch(?: with upstream "([^"]+)")?$/) do |name, u
       FileUtils.cp '.git/refs/heads/master', full_upstream
     end
   end
-  run_silent %(git checkout --quiet -B #{name} --track #{upstream})
+  track = type == 'pushed to' ? '--no-track' : '--track'
+  run_silent %(git checkout --quiet -B #{name} #{track} #{upstream})
 end
 
 Given(/^the default branch for "([^"]+)" is "([^"]+)"$/) do |remote, branch|

lib/hub/context.rb
@@ -99,7 +99,7 @@ module Hub
 
     repo_methods = [
       :current_branch,
-      :current_project, :upstream_project,
+      :remote_branch_and_project,
       :repo_owner, :repo_host,
       :remotes, :remotes_group, :origin_remote
     ]
@@ -141,15 +141,13 @@ module Hub
         remote = origin_remote and remote.project
       end
 
-      def upstream_project
-        if branch = current_branch and upstream = branch.upstream and upstream.remote?
-          remote = remote_by_name upstream.remote_name
-          remote.project
+      def remote_branch_and_project(username_fetcher)
+        project = main_project
+        if project and branch = current_branch
+          branch = branch.push_target(username_fetcher.call(project.host))
+          project = remote_by_name(branch.remote_name).project if branch && branch.remote?
         end
-      end
-
-      def current_project
-        upstream_project || main_project
+        [branch, project]
       end
 
       def current_branch
@@ -165,14 +163,23 @@ module Hub
         Branch.new(self, default_branch || 'refs/heads/master')
       end
 
+      ORIGIN_NAMES = %w[ upstream github origin ]
+
       def remotes
         @remotes ||= begin
           # TODO: is there a plumbing command to get a list of remotes?
           list = git_command('remote').to_s.split("\n")
-          # force "origin" to be first in the list
-          main = list.delete('origin') and list.unshift(main)
+          list = ORIGIN_NAMES.inject([]) { |sorted, name|
+            sorted << list.delete(name)
+          }.compact.concat(list)
           list.map { |name| Remote.new self, name }
         end
+      end
+
+      def remotes_for_publish(owner_name)
+        list = ORIGIN_NAMES.map {|n| remote_by_name(n) }
+        list << remotes.find {|r| r.project.owner == owner_name }
+        list.compact.uniq.reverse
       end
 
       def remotes_group(name)
@@ -316,6 +323,21 @@ module Hub
       def upstream
         if branch = local_repo.git_command("rev-parse --symbolic-full-name #{short_name}@{upstream}")
           Branch.new local_repo, branch
+        end
+      end
+
+      def push_target(owner_name)
+        push_default = local_repo.git_config('push.default')
+        if %w[upstream tracking].include?(push_default)
+          upstream
+        else
+          short = short_name
+          refs = local_repo.remotes_for_publish(owner_name).map { |remote|
+            "refs/remotes/#{remote}/#{short}"
+          }
+          if branch = refs.detect {|ref| local_repo.git_command("rev-parse -q --verify #{ref}") }
+            Branch.new(local_repo, branch)
+          end
         end
       end
 

test/hub_test.rb
@@ -87,7 +87,10 @@ class HubTest < Minitest::Test
       'config --get --bool hub.http-clone' => 'false',
       'config --get hub.protocol' => nil,
       'config --get-all hub.host' => nil,
+      'config --get push.default' => nil,
       'rev-parse -q --git-dir' => '.git'
+
+    stub_remote_branch('origin/master')
   end
 
   def teardown
@@ -297,11 +300,12 @@ class HubTest < Minitest::Test
   end
 
   def test_pullrequest_from_branch_tracking_local
+    stub_config_value 'push.default', 'upstream'
     stub_branch('refs/heads/feature')
     stub_tracking('feature', 'refs/heads/master')
 
     stub_request(:post, "https://api.github.com/repos/defunkt/hub/pulls").
-      with(:body => {'base' => "master", 'head' => "tpw:feature", 'title' => "hereyougo" }).
+      with(:body => {'base' => "master", 'head' => "defunkt:feature", 'title' => "hereyougo" }).
       to_return(:body => mock_pullreq_response(1))
 
     expected = "https://github.com/defunkt/hub/pull/1\n"
@@ -312,6 +316,7 @@ class HubTest < Minitest::Test
     stub_hub_host('git.my.org')
     stub_repo_url('git@git.my.org:defunkt/hub.git')
     stub_branch('refs/heads/feature')
+    stub_remote_branch('origin/feature')
     stub_tracking_nothing('feature')
     stub_command_output "rev-list --cherry-pick --right-only --no-merges origin/feature...", nil
     edit_hub_config do |data|
@@ -319,7 +324,7 @@ class HubTest < Minitest::Test
     end
 
     stub_request(:post, "https://git.my.org/api/v3/repos/defunkt/hub/pulls").
-      with(:body => {'base' => "master", 'head' => "myfiname:feature", 'title' => "hereyougo" }).
+      with(:body => {'base' => "master", 'head' => "defunkt:feature", 'title' => "hereyougo" }).
       to_return(:body => mock_pullreq_response(1, 'api/v3/defunkt/hub', 'git.my.org'))
 
     expected = "https://git.my.org/api/v3/defunkt/hub/pull/1\n"
@@ -485,6 +490,10 @@ class HubTest < Minitest::Test
 
     def stub_tracking_nothing(from = 'master')
       stub_tracking(from, nil)
+    end
+
+    def stub_remote_branch(branch, sha = 'abc123')
+      stub_command_output "rev-parse -q --verify refs/remotes/#{branch}", sha
     end
 
     def stub_remotes_group(name, value)

commit 53152cbdf737b2b3a27c34dff8e940405836c7b7

Fix `ci-status` broken by changes ported from stable branch
Broken by 06999d9

lib/hub/commands.rb
@@ -80,7 +80,7 @@ module Hub
       ref = args.words.first || 'HEAD'
       verbose = args.include?('-v')
 
-      unless head_project = local_repo.current_project
+      unless project = local_repo.main_project
         abort "Aborted: the origin remote doesn't point to a GitHub repository."
       end
 
@@ -88,7 +88,7 @@ module Hub
         abort "Aborted: no revision could be determined from '#{ref}'"
       end
 
-      statuses = api_client.statuses(head_project, sha)
+      statuses = api_client.statuses(project, sha)
       status = statuses.first
       if status
         ref_state = status['state']