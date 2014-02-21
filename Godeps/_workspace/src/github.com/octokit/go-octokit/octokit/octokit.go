8ac437e1d4fed8ea481d7c42b5ee4a66fd40f8f1
Jingwen Owen Ou <jingweno@gmail.com>
2014-02-21 10:17:09 -0800

    Bump go-octokit

--- Godeps/_workspace/src/github.com/octokit/go-octokit/octokit/octokit.go
+++ Godeps/_workspace/src/github.com/octokit/go-octokit/octokit/octokit.go
@@ -4,5 +4,5 @@ const (
 	gitHubAPIURL     = "https://api.github.com"
 	userAgent        = "Octokit Go " + version
 	version          = "0.3.0"
-	defaultMediaType = "application/vnd.github.v3+json; charset=utf-8"
+	defaultMediaType = "application/vnd.github.v3+json;charset=utf-8"
 )
