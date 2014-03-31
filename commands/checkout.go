53a7829747747b1ed210deebae03d308b48d600e
Jingwen Owen Ou <jingweno@gmail.com>
2014-03-31 13:01:05 -0700

    Minor refactor of checkout

--- commands/checkout.go
+++ commands/checkout.go
@@ -2,9 +2,11 @@ package commands
 
 import (
 	"fmt"
+	"log"
+	"regexp"
+
 	"github.com/github/hub/github"
 	"github.com/github/hub/utils"
-	"regexp"
 )
 
 var cmdCheckout = &Command{
@@ -39,23 +41,28 @@ func checkout(command *Command, args *Args) {
 
 func transformCheckoutArgs(args *Args) error {
 	words := args.Words()
+
 	if len(words) == 0 {
 		return nil
 	}
 
 	checkoutURL := words[0]
-	url, err := github.ParseURL(checkoutURL)
-	if err != nil {
-		return nil
-	}
 	var newBranchName string
 	if len(words) > 1 {
 		newBranchName = words[1]
+	}
+ 
+
+	url, err := github.ParseURL(checkoutURL)
+	if err != nil {
+		// not a valid GitHub URL
+		return nil
 	}
 
 	pullURLRegex := regexp.MustCompile("^pull/(\\d+)")
 	projectPath := url.ProjectPath()
 	if !pullURLRegex.MatchString(projectPath) {
+		// not a valid PR URL
 		return nil
 	}
 
