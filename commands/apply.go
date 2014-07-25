6970ef4986f02a4e67911a3f9efdad17ce7f70db
Jingwen Owen Ou <jingweno@gmail.com>
2014-07-25 14:55:12 -0700

    Alias `apply` to `am`

--- commands/apply.go
+++ commands/apply.go
@@ -22,8 +22,22 @@ patch to the working copy.
 `,
 }
 
+var cmdAm = &Command{
+	Run:          apply,
+	GitExtension: true,
+	Usage:        "am GITHUB-URL",
+	Short:        "Apply a patch to files and/or to the index",
+	Long: `Downloads the patch file for the pull request or commit at the URL and
+applies that patch from disk with git am or git apply. Similar to
+cherry-pick, but doesn't add new remotes. git am creates commits while
+preserving authorship info while <code>apply</code> only applies the
+patch to the working copy.
+`,
+}
+
 func init() {
 	CmdRunner.Use(cmdApply)
+	CmdRunner.Use(cmdAm)
 }
 
 /*
