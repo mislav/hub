3474764ea209c920dce96833cc2d0d5fd15227d3
Mislav Marohnić <mislav.marohnic@gmail.com>
2014-07-27 12:35:22 -0700

    Port `hub cherry-pick` tests to cukes

--- test/hub_test.rb
+++ test/hub_test.rb
@@ -116,51 +116,6 @@ class HubTest < Minitest::Test
     WebMock.reset!
   end
 
-  def test_cherry_pick
-    assert_forwarded "cherry-pick a319d88"
-  end
-
-  def test_cherry_pick_url
-    url = 'http://github.com/mislav/hub/commit/a319d88'
-    assert_commands "git fetch mislav", "git cherry-pick a319d88", "cherry-pick #{url}"
-  end
-
-  def test_cherry_pick_url_with_fragment
-    url = 'http://github.com/mislav/hub/commit/abcdef0123456789#comments'
-    assert_commands "git fetch mislav", "git cherry-pick abcdef0123456789", "cherry-pick #{url}"
-  end
-
-  def test_cherry_pick_url_with_remote_add
-    url = 'https://github.com/xoebus/hub/commit/a319d88'
-    assert_commands "git remote add -f xoebus git://github.com/xoebus/hub.git",
-                    "git cherry-pick a319d88",
-                    "cherry-pick #{url}"
-  end
-
-  def test_cherry_pick_origin_url
-    url = 'https://github.com/defunkt/hub/commit/a319d88'
-    assert_commands "git fetch origin", "git cherry-pick a319d88", "cherry-pick #{url}"
-  end
-
-  def test_cherry_pick_github_user_notation
-    assert_commands "git fetch mislav", "git cherry-pick 368af20", "cherry-pick mislav@368af20"
-  end
-
-  def test_cherry_pick_github_user_repo_notation
-    # not supported
-    assert_forwarded "cherry-pick mislav/hubbub@a319d88"
-  end
-
-  def test_cherry_pick_github_notation_too_short
-    assert_forwarded "cherry-pick mislav@a319"
-  end
-
-  def test_cherry_pick_github_notation_with_remote_add
-    assert_commands "git remote add -f xoebus git://github.com/xoebus/hub.git",
-                    "git cherry-pick a319d88",
-                    "cherry-pick xoebus@a319d88"
-  end
-
   def test_pullrequest_alias
     out = hub('e-note')
     assert_equal hub('pull-request'), out
