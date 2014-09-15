9eaa6dd925ff11afc3c69410a860f29e1910379c
Jingwen Owen Ou <jingweno@gmail.com>
2014-09-14 18:36:01 -0700

    Add `yamlConfigEncoder` and `yamlConfigDecoder`

--- github/config_decoder.go
+++ github/config_decoder.go
@@ -2,8 +2,10 @@ package github
 
 import (
 	"io"
+	"io/ioutil"
 
 	"github.com/BurntSushi/toml"
+	"gopkg.in/yaml.v1"
 )
 
 type configDecoder interface {
@@ -16,4 +18,16 @@ type tomlConfigDecoder struct {
 func (t *tomlConfigDecoder) Decode(r io.Reader, v interface{}) error {
 	_, err := toml.DecodeReader(r, v)
 	return err
+}
+
+type yamlConfigDecoder struct {
+}
+
+func (y *yamlConfigDecoder) Decode(r io.Reader, v interface{}) error {
+	d, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+
+	return yaml.Unmarshal(d, v)
 }
--- github/config_encoder.go
+++ github/config_encoder.go
@@ -4,6 +4,7 @@ import (
 	"io"
 
 	"github.com/BurntSushi/toml"
+	"gopkg.in/yaml.v1"
 )
 
 type configEncoder interface {
@@ -16,4 +17,21 @@ type tomlConfigEncoder struct {
 func (t *tomlConfigEncoder) Encode(w io.Writer, v interface{}) error {
 	enc := toml.NewEncoder(w)
 	return enc.Encode(v)
+}
+
+type yamlConfigEncoder struct {
+}
+
+func (y *yamlConfigEncoder) Encode(w io.Writer, v interface{}) error {
+	d, err := yaml.Marshal(v)
+	if err != nil {
+		return err
+	}
+
+	n, err := w.Write(d)
+	if err == nil && n < len(d) {
+		err = io.ErrShortWrite
+	}
+
+	return err
 }
