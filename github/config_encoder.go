5ca19d95a2f25a57f1265a2df7feee781117d533
Jingwen Owen Ou <jingweno@gmail.com>
2014-09-14 19:34:27 -0700

    Load yaml config

--- github/config_encoder.go
+++ github/config_encoder.go
@@ -8,22 +8,31 @@ import (
 )
 
 type configEncoder interface {
-	Encode(w io.Writer, v interface{}) error
+	Encode(w io.Writer, c *Config) error
 }
 
 type tomlConfigEncoder struct {
 }
 
-func (t *tomlConfigEncoder) Encode(w io.Writer, v interface{}) error {
+func (t *tomlConfigEncoder) Encode(w io.Writer, c *Config) error {
 	enc := toml.NewEncoder(w)
-	return enc.Encode(v)
+	return enc.Encode(c)
 }
 
 type yamlConfigEncoder struct {
 }
 
-func (y *yamlConfigEncoder) Encode(w io.Writer, v interface{}) error {
-	d, err := yaml.Marshal(v)
+func (y *yamlConfigEncoder) Encode(w io.Writer, c *Config) error {
+	yc := make(yamlConfig)
+	for _, h := range c.Hosts {
+		yc[h.Host] = yamlHost{
+			User:       h.User,
+			OAuthToken: h.AccessToken,
+			Protocol:   h.Protocol,
+		}
+	}
+
+	d, err := yaml.Marshal(yc)
 	if err != nil {
 		return err
 	}
--- github/config_service_test.go
+++ github/config_service_test.go
@@ -10,12 +10,16 @@ import (
 	"github.com/github/hub/fixtures"
 )
 
-func TestConfigService_Load(t *testing.T) {
-	testConfig := fixtures.SetupTestConfigs()
+func TestConfigService_TomlLoad(t *testing.T) {
+	testConfig := fixtures.SetupTomlTestConfig()
 	defer testConfig.TearDown()
 
 	cc := &Config{}
-	err := newConfigService().Load(testConfig.Path, cc)
+	cs := &configService{
+		Encoder: &tomlConfigEncoder{},
+		Decoder: &tomlConfigDecoder{},
+	}
+	err := cs.Load(testConfig.Path, cc)
 	assert.Equal(t, nil, err)
 
 	assert.Equal(t, 1, len(cc.Hosts))
@@ -26,7 +30,27 @@ func TestConfigService_Load(t *testing.T) {
 	assert.Equal(t, "http", host.Protocol)
 }
 
-func TestConfigService_Save(t *testing.T) {
+func TestConfigService_YamlLoad(t *testing.T) {
+	testConfig := fixtures.SetupTestConfigs()
+	defer testConfig.TearDown()
+
+	cc := &Config{}
+	cs := &configService{
+		Encoder: &yamlConfigEncoder{},
+		Decoder: &yamlConfigDecoder{},
+	}
+	err := cs.Load(testConfig.Path, cc)
+	assert.Equal(t, nil, err)
+
+	assert.Equal(t, 1, len(cc.Hosts))
+	host := cc.Hosts[0]
+	assert.Equal(t, "github.com", host.Host)
+	assert.Equal(t, "jingweno", host.User)
+	assert.Equal(t, "123", host.AccessToken)
+	assert.Equal(t, "http", host.Protocol)
+}
+
+func TestConfigService_TomlSave(t *testing.T) {
 	file, _ := ioutil.TempFile("", "test-gh-config-")
 	defer os.RemoveAll(file.Name())
 
@@ -38,7 +62,11 @@ func TestConfigService_Save(t *testing.T) {
 	}
 	c := Config{Hosts: []Host{host}}
 
-	err := newConfigService().Save(file.Name(), &c)
+	cs := &configService{
+		Encoder: &tomlConfigEncoder{},
+		Decoder: &tomlConfigDecoder{},
+	}
+	err := cs.Save(file.Name(), &c)
 	assert.Equal(t, nil, err)
 
 	b, _ := ioutil.ReadFile(file.Name())
@@ -47,5 +75,32 @@ func TestConfigService_Save(t *testing.T) {
   user = "jingweno"
   access_token = "123"
   protocol = "https"`
+	assert.Equal(t, content, strings.TrimSpace(string(b)))
+}
+
+func TestConfigService_YamlSave(t *testing.T) {
+	file, _ := ioutil.TempFile("", "test-gh-config-")
+	defer os.RemoveAll(file.Name())
+
+	host := Host{
+		Host:        "github.com",
+		User:        "jingweno",
+		AccessToken: "123",
+		Protocol:    "https",
+	}
+	c := Config{Hosts: []Host{host}}
+
+	cs := &configService{
+		Encoder: &yamlConfigEncoder{},
+		Decoder: &yamlConfigDecoder{},
+	}
+	err := cs.Save(file.Name(), &c)
+	assert.Equal(t, nil, err)
+
+	b, _ := ioutil.ReadFile(file.Name())
+	content := `github.com:
+  user: jingweno
+  oauth_token: "123"
+  protocol: https`
 	assert.Equal(t, content, strings.TrimSpace(string(b)))
 }
