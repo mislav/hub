89ef033c7542767d222502207677d6badf3aa3bc
Jingwen Owen Ou <jingweno@gmail.com>
2014-10-19 12:36:02 -0700

    Print hub version instead of gh

--- commands/version.go
+++ commands/version.go
@@ -2,9 +2,10 @@ package commands
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/github/hub/git"
 	"github.com/github/hub/utils"
-	"os"
 )
 
 const Version = "2.0.0"
@@ -12,8 +13,8 @@ const Version = "2.0.0"
 var cmdVersion = &Command{
 	Run:   runVersion,
 	Usage: "version",
-	Short: "Show gh version",
-	Long:  `Shows git version and gh client version.`,
+	Short: "Show hub version",
+	Long:  `Shows git version and hub client version.`,
 }
 
 func init() {
@@ -24,7 +25,7 @@ func runVersion(cmd *Command, args *Args) {
 	gitVersion, err := git.Version()
 	utils.Check(err)
 
-	ghVersion := fmt.Sprintf("gh version %s", Version)
+	ghVersion := fmt.Sprintf("hub version %s", Version)
 
 	fmt.Println(gitVersion)
 	fmt.Println(ghVersion)
