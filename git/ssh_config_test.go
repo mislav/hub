aab2e5f9d307252e78c286faf3c20ff30642c1cd
Jingwen Owen Ou <jingweno@gmail.com>
2014-11-03 08:26:52 -0800

    Ignore replacing ssh host that fixes limited network
    ssh.github.com is a hack for people on limited networks to be able to use SSH through HTTPS port. We should ignore replacing it when constructing URLs.
    
    As part of the fix, refactor into SSHConfig and SSHConfigReader to make code testable. It also fixes a bug on `hostReStr` that doesn’t match the “Hostname” string in ssh config.
    
    Issue: https://github.com/github/hub/issues/672

---
+++ git/ssh_config_test.go
@@ -0,0 +1,23 @@
+package git
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/bmizerany/assert"
+)
+
+func TestSSHConfigReader_Read(t *testing.T) {
+	f, _ := ioutil.TempFile("", "ssh-config")
+	c := `Host github.com
+  Hostname ssh.github.com
+  Port 443
+	`
+
+	ioutil.WriteFile(f.Name(), []byte(c), os.ModePerm)
+
+	r := &SSHConfigReader{[]string{f.Name()}}
+	sc := r.Read()
+	assert.Equal(t, "ssh.github.com", sc["github.com"])
+}
--- git/url_test.go
+++ git/url_test.go
@@ -1,27 +1,40 @@
 package git
 
 import (
-	"github.com/bmizerany/assert"
 	"testing"
+
+	"github.com/bmizerany/assert"
 )
 
-func TestURL_ParseURL(t *testing.T) {
-	u, err := ParseURL("https://github.com/octokit/go-octokit.git")
+func TestURLParser_ParseURL(t *testing.T) {
+	c := make(SSHConfig)
+	c["github.com"] = "ssh.github.com"
+	c["git.company.com"] = "ssh.git.company.com"
+
+	p := &URLParser{c}
+
+	u, err := p.Parse("https://github.com/octokit/go-octokit.git")
 	assert.Equal(t, nil, err)
 	assert.Equal(t, "github.com", u.Host)
 	assert.Equal(t, "https", u.Scheme)
 	assert.Equal(t, "/octokit/go-octokit.git", u.Path)
 
-	u, err = ParseURL("git://github.com/octokit/go-octokit.git")
+	u, err = p.Parse("git://github.com/octokit/go-octokit.git")
 	assert.Equal(t, nil, err)
 	assert.Equal(t, "github.com", u.Host)
 	assert.Equal(t, "git", u.Scheme)
 	assert.Equal(t, "/octokit/go-octokit.git", u.Path)
 
-	u, err = ParseURL("git@github.com:lostisland/go-sawyer.git")
+	u, err = p.Parse("git@github.com:lostisland/go-sawyer.git")
 	assert.Equal(t, nil, err)
 	assert.Equal(t, "github.com", u.Host)
 	assert.Equal(t, "ssh", u.Scheme)
 	assert.Equal(t, "git", u.User.Username())
 	assert.Equal(t, "/lostisland/go-sawyer.git", u.Path)
+
+	u, err = p.Parse("https://git.company.com/octokit/go-octokit.git")
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "ssh.git.company.com", u.Host)
+	assert.Equal(t, "https", u.Scheme)
+	assert.Equal(t, "/octokit/go-octokit.git", u.Path)
 }
