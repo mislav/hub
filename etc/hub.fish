3bd2f2bc18eff1ccbab18f29687ceeb37dcc287c
hasit <hasit@uw.edu>
2015-05-26 16:10:30 -0700

    2 support functions, help and fork tab completeions added

--- etc/hub.fish
+++ etc/hub.fish
@@ -1,2 +1,29 @@
 # fish completion for hub
 # Since git is aliased as hub, need to check if `command hub` creates any conflict with git command completions
+
+# statement starting with 'hub'
+function __fish_hub_needs_command
+    set cmd (commandline -opc)
+    if [ (count $cmd) -eq 1 -a $cmd[1] = 'hub' ]
+        return 0
+    end
+    return 1
+end
+
+# statement starting with 'hub <command>'
+function __fish_hub_using_command
+    set cmd (commandline -opc)
+    if [ (count $cmd) -gt 1 ]
+        if [ $argv[1] = $cmd[2] ]
+            return 0
+        end
+    end
+    return 1
+end
+
+# help
+complete -f -c hub -n '__fish_hub_needs_command' -a help -d 'Display enhanced git-help(1)'
+
+# fork
+complete -f -c hub -n '__fish_hub_needs_command' -a fork -d 'Fork the original project on GitHub and add a new remote for it under your username.'
+complete -f -c hub -n '__fish_hub_using_command fork' -l no-remote -d 'Fork the original project with no remote'
