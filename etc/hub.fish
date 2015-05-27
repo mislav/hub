32f7aa88c72a9409762f09837b732804018668d5
hasit <hasit@uw.edu>
2015-05-27 10:59:13 -0700

    --help available for every command

--- etc/hub.fish
+++ etc/hub.fish
@@ -1,6 +1,11 @@
 # fish completion for hub
 # Since git is aliased as hub, need to check if `command hub` creates any conflict with git command completions
 
+# TODO
+# Snippet type thing. 'hub create [name]' where [name] should be in grey and suppress the smart suggesstions from fish.
+# 'hub help <tab>' should show a list of commands that help is available for.
+# '__fish_hub_suppress_files' function.
+
 # statement starting with 'hub'
 function __fish_hub_needs_command
     set cmd (commandline -opc)
@@ -22,11 +27,11 @@ function __fish_hub_using_command
 end
 
 # help
-# 'hub help <tab>' should show a list of all the commands just like 'hub <tab>'
+# show '--help' for every command
 complete -f -c hub -n '__fish_hub_needs_command' -a help -d 'Display enhanced git-help(1)'
+complete -f -c hub -n 'not __fish_hub_needs_command' -l help -d 'Display enhanced git-help(1)'
 #complete -f -c hub -n 'not __fish_hub_needs_command' -l help -d 'Display enhanced git-help(1)'
 
 # fork
 complete -f -c hub -n '__fish_hub_needs_command' -a fork -d 'Fork the original project on GitHub and add a new remote for it under your username'
 complete -f -c hub -n '__fish_hub_using_command fork' -l no-remote -d 'Fork the original project with no remote'
-complete -f -c hub -n '__fish_hub_using_command fork' -l help -d 'Display enhanced git-help(1)'
