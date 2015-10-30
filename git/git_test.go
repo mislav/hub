65f12fb962b3cb62e1055b5402edd2d2488fecd2
Madhusudan.C.S <madhusudancs@gmail.com>
2015-10-29 22:55:36 -0700

    Add unit tests for git.Remotes() and github.Remotes().
    Also, define AddRemote() method for test Repo fixture.

--- git/git_test.go
+++ git/git_test.go
@@ -1,6 +1,7 @@
 package git
 
 import (
+	"fmt"
 	"os"
 	"strings"
 	"testing"
@@ -91,4 +92,60 @@ func TestGitConfig(t *testing.T) {
 	v, err = GlobalConfig("hub.test")
 	assert.Equal(t, nil, err)
 	assert.Equal(t, "", v)
+}
+
+func TestRemotes(t *testing.T) {
+	repo := fixtures.SetupTestRepo()
+	defer repo.TearDown()
+
+	type remote struct {
+		name    string
+		url     string
+		pushUrl string
+	}
+	testCases := map[string]remote{
+		"testremote1": {
+			"testremote1",
+			"https://example.com/test1/project1.git",
+			"no_push",
+		},
+		"testremote2": {
+			"testremote2",
+			"user@example.com:test2/project2.git",
+			"http://example.com/project.git",
+		},
+		"testremote3": {
+			"testremote3",
+			"https://example.com/test1/project2.git",
+			"",
+		},
+	}
+
+	for _, tc := range testCases {
+		repo.AddRemote(tc.name, tc.url, tc.pushUrl)
+	}
+
+	remotes, err := Remotes()
+	assert.Equal(t, nil, err)
+
+	// In addition to the remotes we added to the repo, repo will
+	// also have an additional remote "origin". So add it to the
+	// expected cases to test.
+	wantCases := map[string]struct{}{
+		fmt.Sprintf("origin	%s (fetch)", repo.Remote): {},
+		fmt.Sprintf("origin	%s (push)", repo.Remote): {},
+		"testremote1	https://example.com/test1/project1.git (fetch)": {},
+		"testremote1	no_push (push)": {},
+		"testremote2	user@example.com:test2/project2.git (fetch)": {},
+		"testremote2	http://example.com/project.git (push)": {},
+		"testremote3	https://example.com/test1/project2.git (fetch)": {},
+		"testremote3	https://example.com/test1/project2.git (push)": {},
+	}
+
+	assert.Equal(t, len(remotes), len(wantCases))
+	for _, got := range remotes {
+		if _, ok := wantCases[got]; !ok {
+			t.Errorf("Unexpected remote: %s", got)
+		}
+	}
 }
