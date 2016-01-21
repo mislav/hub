1e0b6235890cf5747b1760d78ff0876af90f5499
Mislav Marohnić <mislav.marohnic@gmail.com>
2016-01-22 01:26:55 +1100

    Handle all sorts of weird git remote URLs
    - git+ssh://github.com/...
    - ssh://github.com:22/...
    - git@github.com:/...
    
    Fixes #876

--- github/remote_test.go
+++ github/remote_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/github/hub/fixtures"
 )
 
-func TestGithubRepo_Remotes(t *testing.T) {
+func TestGithubRemote_NoPush(t *testing.T) {
 	repo := fixtures.SetupTestRepo()
 	defer repo.TearDown()
 
@@ -21,6 +21,60 @@ func TestGithubRepo_Remotes(t *testing.T) {
 	assert.Equal(t, remotes[0].URL.Scheme, "ssh")
 	assert.Equal(t, remotes[0].URL.Host, "example.com")
 	assert.Equal(t, remotes[0].URL.Path, "/test/project.git")
+	assert.Equal(t, remotes[1].Name, "origin")
+	assert.Equal(t, remotes[1].URL.Path, repo.Remote)
+}
+
+func TestGithubRemote_GitPlusSsh(t *testing.T) {
+	repo := fixtures.SetupTestRepo()
+	defer repo.TearDown()
+
+	remoteName := "upstream"
+	repo.AddRemote(remoteName, "git+ssh://git@github.com/frozencemetery/python-gssapi", "")
+
+	remotes, err := Remotes()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, len(remotes), 2)
+	assert.Equal(t, remotes[0].Name, remoteName)
+	assert.Equal(t, remotes[0].URL.Scheme, "ssh")
+	assert.Equal(t, remotes[0].URL.Host, "github.com")
+	assert.Equal(t, remotes[0].URL.Path, "/frozencemetery/python-gssapi")
+	assert.Equal(t, remotes[1].Name, "origin")
+	assert.Equal(t, remotes[1].URL.Path, repo.Remote)
+}
+
+func TestGithubRemote_SshPort(t *testing.T) {
+	repo := fixtures.SetupTestRepo()
+	defer repo.TearDown()
+
+	remoteName := "upstream"
+	repo.AddRemote(remoteName, "ssh://git@github.com:22/hakatashi/dotfiles.git", "")
+
+	remotes, err := Remotes()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, len(remotes), 2)
+	assert.Equal(t, remotes[0].Name, remoteName)
+	assert.Equal(t, remotes[0].URL.Scheme, "ssh")
+	assert.Equal(t, remotes[0].URL.Host, "github.com")
+	assert.Equal(t, remotes[0].URL.Path, "/hakatashi/dotfiles.git")
+	assert.Equal(t, remotes[1].Name, "origin")
+	assert.Equal(t, remotes[1].URL.Path, repo.Remote)
+}
+
+func TestGithubRemote_ColonSlash(t *testing.T) {
+	repo := fixtures.SetupTestRepo()
+	defer repo.TearDown()
+
+	remoteName := "upstream"
+	repo.AddRemote(remoteName, "git@github.com:/fatso83/my-project.git", "")
+
+	remotes, err := Remotes()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, len(remotes), 2)
+	assert.Equal(t, remotes[0].Name, remoteName)
+	assert.Equal(t, remotes[0].URL.Scheme, "ssh")
+	assert.Equal(t, remotes[0].URL.Host, "github.com")
+	assert.Equal(t, remotes[0].URL.Path, "/fatso83/my-project.git")
 	assert.Equal(t, remotes[1].Name, "origin")
 	assert.Equal(t, remotes[1].URL.Path, repo.Remote)
 }
