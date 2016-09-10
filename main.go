7afbe420536e31729ccbdd7ee5d290e911d277d3
Mislav Marohnić <mislav.marohnic@gmail.com>
2016-09-10 23:08:48 +0200

    Show error to stderr if shelling out failed
    If shelling out to nonexistent command, such as `echo` on Windows, print
    the error on stderr instead of failing silently. This is also helpful
    when hub tries to forward a command to `git`, but git is not installed.

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"os"
+
 	"github.com/github/hub/commands"
 	"github.com/github/hub/github"
-	"os"
+	"github.com/github/hub/ui"
 )
 
 func main() {
 	defer github.CaptureCrash()
 
 	err := commands.CmdRunner.Execute()
+	if !err.Ran {
+		ui.Errorln(err.Error())
+	}
 	os.Exit(err.ExitCode)
 }
