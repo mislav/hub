fb99671889d123b06092696c09b7c6abb2c1116c
boris <boris.schmidt@rea-group.com>
2016-10-14 16:23:12 +0200

    Update package

--- vendor/github.com/boris-rea/hub/commands/flow.go
+++ vendor/github.com/boris-rea/hub/commands/flow.go
@@ -18,10 +18,22 @@ var (
 		Key: "feature",
 		Run: flowFeature,
 	}
+
+	cmdFlowRelease = &Command{
+		Key: "release",
+		Run: flowRelease,
+	}
+
+	cmdFlowHotfix = &Command{
+		Key: "hotfix",
+		Run: flowHotfix,
+	}
 )
 
 func init() {
 	cmdFlow.Use(cmdFlowFeature)
+	cmdFlow.Use(cmdFlowRelease)
+	cmdFlow.Use(cmdFlowHotfix)
 	CmdRunner.Use(cmdFlow)
 }
 
@@ -45,6 +57,70 @@ func flowFeature(command *Command, args *Args) {
 		}
 	case "finish":
 		err := gitFlow.FlowFeatureFinish(featureName)
+		if err != nil {
+			errorMessage = err.Error()
+		}
+	default:
+		errorMessage = cmdFlow.HelpText()
+	}
+
+	if errorMessage != "" {
+		utils.Check(fmt.Errorf("%s", errorMessage))
+	}
+}
+
+func flowRelease(command *Command, args *Args) {
+	args.NoForward()
+	words := args.Words()
+
+	if len(words) != 2 {
+		utils.Check(fmt.Errorf("%s", cmdFlow.HelpText()))
+	}
+
+	errorMessage := ""
+	instruction := words[0]
+	featureName := words[1]
+
+	switch instruction {
+	case "start":
+		err := gitFlow.FlowReleaseStart(featureName)
+		if err != nil {
+			errorMessage = err.Error()
+		}
+	case "finish":
+		err := gitFlow.FlowReleaseFinish(featureName)
+		if err != nil {
+			errorMessage = err.Error()
+		}
+	default:
+		errorMessage = cmdFlow.HelpText()
+	}
+
+	if errorMessage != "" {
+		utils.Check(fmt.Errorf("%s", errorMessage))
+	}
+}
+
+func flowHotfix(command *Command, args *Args) {
+	args.NoForward()
+	words := args.Words()
+
+	if len(words) != 2 {
+		utils.Check(fmt.Errorf("%s", cmdFlow.HelpText()))
+	}
+
+	errorMessage := ""
+	instruction := words[0]
+	featureName := words[1]
+
+	switch instruction {
+	case "start":
+		err := gitFlow.FlowHotfixStart(featureName)
+		if err != nil {
+			errorMessage = err.Error()
+		}
+	case "finish":
+		err := gitFlow.FlowHotfixFinish(featureName)
 		if err != nil {
 			errorMessage = err.Error()
 		}
--- vendor/github.com/boris-rea/hub/flow/feature.go
+++ vendor/github.com/boris-rea/hub/flow/feature.go
@@ -1,7 +1,5 @@
 package flow
 
-import "github.com/github/hub/git"
-
 func FlowFeatureStart(featureName string) (err error) {
 	branchName := "feature/" + featureName
 
@@ -23,24 +21,12 @@ func FlowFeatureFinish(featureName string) (err error) {
 	cmdGit := [][]string{}
 
 	cmdGit1 := []string{"checkout", "develop"}
-	cmdGit2 := []string{"merge", branchName}
+	cmdGit2 := []string{"merge", branchName, "--no-ff"}
 	cmdGit3 := []string{"branch", "-d", branchName}
 
 	cmdGit = append(cmdGit, cmdGit1, cmdGit2, cmdGit3)
 
 	err = launchCmdGit(cmdGit)
-
-	return
-}
-
-func launchCmdGit(cmdGit [][]string) (err error) {
-	for i := range cmdGit {
-		err = git.Spawn(cmdGit[i]...)
-
-		if err != nil {
-			break
-		}
-	}
 
 	return
 }
