a7bfe5fb33b08645ee7ef7bc8b5b2521623f5b8b
Mat Schaffer <mat@schaffer.me>
2016-10-28 10:03:21 +0900

    Switch default remote URL to https
    Github doesn't seem to honor `git://` anymore.

--- github/project.go
+++ github/project.go
@@ -72,12 +72,12 @@ func (p *Project) GitURL(name, owner string, isSSH bool) (url string) {
 
 	host := rawHost(p.Host)
 
-	if preferredProtocol() == "https" {
-		url = fmt.Sprintf("https://%s/%s/%s.git", host, owner, name)
+	if preferredProtocol() == "git" {
+		url = fmt.Sprintf("git://%s/%s/%s.git", host, owner, name)
 	} else if isSSH || preferredProtocol() == "ssh" {
 		url = fmt.Sprintf("git@%s:%s/%s.git", host, owner, name)
 	} else {
-		url = fmt.Sprintf("git://%s/%s/%s.git", host, owner, name)
+		url = fmt.Sprintf("https://%s/%s/%s.git", host, owner, name)
 	}
 
 	return url
