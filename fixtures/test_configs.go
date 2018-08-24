0eb76ff86f679b1b58385c33982f99b68b60e9d8
Alexander Pakulov <apakulov@stripe.com>
2018-08-24 10:34:07 -0700

    Separate config and tests for unix-socket

--- fixtures/test_configs.go
+++ fixtures/test_configs.go
@@ -21,6 +21,20 @@ func SetupTomlTestConfig() *TestConfigs {
   host = "github.com"
   user = "jingweno"
   access_token = "123"
+  protocol = "http"`
+	ioutil.WriteFile(file.Name(), []byte(content), os.ModePerm)
+	os.Setenv("HUB_CONFIG", file.Name())
+
+	return &TestConfigs{file.Name()}
+}
+
+func SetupTomlTestConfigWithUnixSocket() *TestConfigs {
+	file, _ := ioutil.TempFile("", "test-gh-config-")
+
+	content := `[[hosts]]
+  host = "github.com"
+  user = "jingweno"
+  access_token = "123"
   protocol = "http"
   unix_socket = "/tmp/go.sock"`
 	ioutil.WriteFile(file.Name(), []byte(content), os.ModePerm)
@@ -30,6 +44,20 @@ func SetupTomlTestConfig() *TestConfigs {
 }
 
 func SetupTestConfigs() *TestConfigs {
+	file, _ := ioutil.TempFile("", "test-gh-config-")
+
+	content := `---
+github.com:
+- user: jingweno
+  oauth_token: 123
+  protocol: http`
+	ioutil.WriteFile(file.Name(), []byte(content), os.ModePerm)
+	os.Setenv("HUB_CONFIG", file.Name())
+
+	return &TestConfigs{file.Name()}
+}
+
+func SetupTestConfigsWithUnixSocket() *TestConfigs {
 	file, _ := ioutil.TempFile("", "test-gh-config-")
 
 	content := `---
