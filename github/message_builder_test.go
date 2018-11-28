e0dba3e14771ec2291029c606136ae5fc74ab399
Mislav Marohnić <mislav.marohnic@gmail.com>
2018-11-28 02:29:30 +0100

    Add test for multiline title handling

---
+++ github/message_builder_test.go
@@ -0,0 +1,23 @@
+package github
+
+import (
+	"testing"
+
+	"github.com/bmizerany/assert"
+)
+
+func TestMessageBuilder_multiline_title(t *testing.T) {
+	builder := &MessageBuilder{
+		Message: `hello
+multiline
+text
+
+the rest is
+description`,
+	}
+
+	title, body, err := builder.Extract()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "hello multiline text", title)
+	assert.Equal(t, "the rest is\ndescription", body)
+}
