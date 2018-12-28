2b73d4faed448817b3b6b5754c1e514017e055ad
Mislav Marohnić <mislav.marohnic@gmail.com>
2018-12-28 07:21:03 +0100

    Document how we scan git remotes and branch tracking information

--- commands/pull_request.go
+++ commands/pull_request.go
@@ -60,11 +60,15 @@ pull-request -i <ISSUE>
 		Push the current branch to <HEAD> before creating the pull request.
 
 	-b, --base=<BASE>
-		The base branch in "[OWNER:]BRANCH" format. Defaults to the default branch
-		(usually "master").
+		The base branch in the "[<OWNER>:]<BRANCH>" format. Defaults to the default
+		branch of the upstream repository (usually "master").
+
+		See the "CONVENTIONS" section of hub(1) for more information on how hub
+		selects the defaults in case of multiple git remotes.
 
 	-h, --head=<HEAD>
-		The head branch in "[OWNER:]BRANCH" format. Defaults to the current branch.
+		The head branch in "[<OWNER>:]<BRANCH>" format. Defaults to the currently
+		checked out branch.
 
 	-r, --reviewer=<USERS>
 		A comma-separated list of GitHub handles to request a review from.
