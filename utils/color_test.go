a7bec44eeca440f48a99590c2874debb25cd540a
Mislav Marohnić <mislav.marohnic@gmail.com>
2019-01-03 02:47:14 +0100

    Restore compatibility with Go 1.8/1.9

--- utils/color_test.go
+++ utils/color_test.go
@@ -11,3 +11,21 @@ func TestColorBrightness(t *testing.T) {
 	actual := c.Brightness()
 	assert.Equal(t, float32(0.15946665406227112), actual)
 }
+
+func TestRoundDown(t *testing.T) {
+	assert.Equal(t, 3.0, round(3.0))
+	assert.Equal(t, 3.0, round(3.01))
+	assert.Equal(t, 3.0, round(3.49))
+}
+
+func TestRoundUp(t *testing.T) {
+	assert.Equal(t, 3.0, round(2.5))
+	assert.Equal(t, 3.0, round(2.51))
+	assert.Equal(t, 3.0, round(2.99))
+}
+
+func TestRoundNegative(t *testing.T) {
+	assert.Equal(t, -2.0, round(-2.49))
+	assert.Equal(t, -3.0, round(-2.5))
+	assert.Equal(t, -3.0, round(-2.51))
+}
