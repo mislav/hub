67a98b4d859b6089f4b865a306befeb8b3603153
Mislav Marohnić <mislav.marohnic@gmail.com>
2019-09-23 12:31:14 +0200

    [auth] Fix password prompt on Windows
    It was wrong to just assume that stdin is "0" everywhere.
    
    Followup to 7514fa2b09ced30aa00468ba3a2aaa3bda39d6ff
    
    Fixes #2276

--- github/config.go
+++ github/config.go
@@ -192,7 +192,7 @@ func (c *Config) scanLine() string {
 }
 
 func getPassword() (string, error) {
-	stdin := 0 // syscall.Stdin
+	stdin := int(syscall.Stdin)
 	initialTermState, err := terminal.GetState(stdin)
 	if err != nil {
 		return "", err
