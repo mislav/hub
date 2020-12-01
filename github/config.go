7e3e213f21d81c9e58fb2a959dbccb25d7b5a329
Cuong Manh Le <cuong@orijtech.com>
2020-12-01 13:13:35 +0700

    github: fix unbuffered channel passed to signal.Notify
    The docs at os/signal.Notify warn about this signal delivery loss bug at
    https://golang.org/pkg/os/signal/#Notify, which says:
    
        Package signal will not block sending to c: the caller must ensure
        that c has sufficient buffer space to keep up with the expected signal
        rate. For a channel used for notification of just one signal value,
        a buffer of size 1 is sufficient.
    
    Caught by a static analysis tool from Orijtech, Inc. called "sigchanyzer"

--- github/config.go
+++ github/config.go
@@ -198,7 +198,7 @@ func getPassword() (string, error) {
 		return "", err
 	}
 
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		s := <-c
